ca1230928aca59d49bd840b4522442a9db315397
ruhaim <ruhaim@gmail.com>
2024-12-10 08:51:08 +0800

    chore: connect main app wiith provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { makeServer } from './server.ts'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 makeServer()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
