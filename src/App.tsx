5f149d6c641cdc8302693a83cec38110f1d66b28
ruhaim <ruhaim@gmail.com>
2024-12-10 08:53:34 +0800

    feat: connect Userdashboard in main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { UserDashboard } from './feature/users/components/UserDashboard'
+
 
 function App() {
   const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -17,6 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <UserDashboard/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
