d766ab6c38cfcd35f67f26e39f1fb2dd390b9e7d
ruhaim <ruhaim@gmail.com>
2024-12-10 08:48:53 +0800

    chore: add middleware to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,8 @@ export const store = configureStore({
     [userPageState.reducerPath]: userPageState.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
