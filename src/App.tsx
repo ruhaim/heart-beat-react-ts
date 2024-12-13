ba0a4707d61c7de5f60195ae83f1d17eb7fff9a3
ruhaim <ruhaim@gmail.com>
2024-12-13 09:19:13 +0800

    chore: depend main layout on viewport height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+
 import { UserDashboard } from './feature/users/components/UserDashboard'
 import { AppBar, Box, Container, Grid2 as Grid, Toolbar, Typography, styled } from '@mui/material'
 import { DarkModeToggler } from './theme/DarkModeToggler'
+import { BarChart } from './feature/users/viz/BarChart'
 
 function App() {
 
   return (
     <>
+
       <AppBar position="fixed">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -16,21 +18,23 @@ function App() {
         </Toolbar>
       </AppBar>
 
-      <Container sx={{ marginTop: "4em" }}>
-
-        <Grid container spacing={2} columns={16}>
-          <Grid size={8}>
-            <Box>Box 1</Box>
+      <Container>
+        <Toolbar />
+        <Grid container spacing={2} columns={16} >
+          <Grid size={8} sx={{ height: '25vh' }}>
+            <BarChart />
           </Grid>
           <Grid size={8}>
             <Box>Box 2</Box>
           </Grid>
-          <Grid size={16}>
+          <Grid size={16} sx={{ height: '58vh' }}>
             <UserDashboard />
           </Grid>
         </Grid>
-
       </Container>
+
+
+
 
     </>
   )
