d91ffe4234139ebd440c8b138ca417263fdb4d5b
Ruha <ruhaim@gmail.com>
2024-12-14 10:37:27 +0800

    chore: fix lint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import {
+  AppBar,
+  Container,
+  Grid2 as Grid,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-import { UserDashboard } from './feature/users/components/UserDashboard'
-import { AppBar, Box, Container, Grid2 as Grid, Toolbar, Typography, styled } from '@mui/material'
-import { DarkModeToggler } from './theme/DarkModeToggler'
-import { BarChart } from './feature/users-vis/components/BarChart'
-import { GenderDistributionChart } from './feature/users-vis/components/GenderDistributionChart'
-import { CityDistributionChart } from './feature/users-vis/components/CityDistributionChart'
+import { UserDashboard } from "./feature/users/components/UserDashboard";
+import { CityDistributionChart } from "./feature/users-vis/components/CityDistributionChart";
+import { GenderDistributionChart } from "./feature/users-vis/components/GenderDistributionChart";
+import { DarkModeToggler } from "./theme/DarkModeToggler";
 
 function App() {
-
   return (
     <>
-
       <AppBar position="fixed">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -22,24 +25,20 @@ function App() {
 
       <Container>
         <Toolbar />
-        <Grid container spacing={2} columns={16} >
-          <Grid size={8} sx={{ height: '25vh' }}>
+        <Grid container spacing={2} columns={16}>
+          <Grid size={8} sx={{ height: "25vh" }}>
             <GenderDistributionChart />
           </Grid>
-          <Grid size={8} sx={{ height: '25vh' }}>
+          <Grid size={8} sx={{ height: "25vh" }}>
             <CityDistributionChart />
           </Grid>
-          <Grid size={16} sx={{ height: '58vh' }}>
+          <Grid size={16} sx={{ height: "58vh" }}>
             <UserDashboard />
           </Grid>
         </Grid>
       </Container>
-
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
