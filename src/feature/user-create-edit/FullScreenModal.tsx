16ef46fbfc38d9b72c1b52d8a23a3fab5fd7268b
ruhaim <ruhaim@gmail.com>
2024-12-16 12:16:40 +0800

    feature: add uder edit forms

---
+++ src/feature/user-create-edit/FullScreenModal.tsx
@@ -0,0 +1,76 @@
+import React, { FC, PropsWithChildren } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Dialog, { DialogProps } from "@mui/material/Dialog";
+import IconButton from "@mui/material/IconButton";
+import Slide from "@mui/material/Slide";
+import Toolbar from "@mui/material/Toolbar";
+import { TransitionProps } from "@mui/material/transitions";
+import Typography from "@mui/material/Typography";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<unknown>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+type FullScreenModalProps = {
+  onClose?: () => void
+  title?: string
+  actionBtnLabel?: string
+  open: boolean
+  onActionButtonClick?: () => void;
+
+} & DialogProps
+
+export const FullScreenModal: FC<PropsWithChildren<FullScreenModalProps>> = ({
+  children,
+  onClose,
+  title,
+  actionBtnLabel = "Save",
+  open,
+  onActionButtonClick }) => {
+  const handleClose = () => {
+    onClose?.()
+  };
+  const handleActionButtonClick = () => {
+    if (onActionButtonClick) {
+      return onActionButtonClick()
+    }
+    return onClose?.();
+  }
+  return (
+    <React.Fragment>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              {title}
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleActionButtonClick}>
+              {actionBtnLabel}
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {children}
+      </Dialog>
+    </React.Fragment>
+  );
+};
---
+++ src/feature/user-create-edit/UserEditForm.tsx
@@ -0,0 +1,117 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useFormik } from "formik";
+import { withZodSchema } from "formik-validator-zod";
+import { z } from "zod";
+
+const UseEditFormSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    email: z.string(),
+    gender: z.union([z.literal("male"), z.literal("female")]),
+    dob: z.string(),
+    city: z.string(),
+    mobile: z.string(),
+})
+
+type RegisterFormSchemaType = z.infer<typeof UseEditFormSchema>;
+export default function Page() {
+    const formik = useFormik<RegisterFormSchemaType>({
+        initialValues: {
+            id: 0,
+            name: "",
+            dob: "",
+            email: "",
+            gender: "male",
+            city: "",
+            mobile: ""
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values));
+        },
+        validate: withZodSchema(UseEditFormSchema),
+    });
+    console.log(
+        `Values : ${JSON.stringify(formik.values)}\n\nerrors : ${JSON.stringify(
+            formik.errors
+        )}\n\n`
+    );
+    return (
+        <section>
+            <form
+                onSubmit={formik.handleSubmit}
+
+            >
+                <Typography variant="h6">Edit User</Typography>
+                <TextField
+                    id="name"
+                    type="text"
+                    label="Name"
+                    placeholder="Name"
+                    fullWidth
+                    {...formik.getFieldProps("name")}
+                />
+                {formik.errors.name && formik.touched.name && (
+                    <div >
+                        {formik.errors.name}
+                    </div>
+                )}
+                <TextField
+                    id="dob"
+                    type="date"
+                    label="Date of Birth"
+                    fullWidth
+                    {...formik.getFieldProps("dob")}
+                />
+                {formik.errors.dob && formik.touched.dob && (
+                    <div>
+                        {formik.errors.dob}
+                    </div>
+                )}
+                <TextField
+                    id="email"
+                    type="email"
+                    label="Email"
+                    placeholder="johndoe@example.com"
+                    fullWidth
+                    {...formik.getFieldProps("email")}
+                />
+                {formik.errors.email && formik.touched.email && (
+                    <div className="text-sm text-red-600 border-2 border-red-600 w-full h-8 bg-red-100 rounded-md flex justify-center items-center">
+                        {formik.errors.email}
+                    </div>
+                )}
+                <TextField
+                    id="city"
+                    type="city"
+                    label="City"
+                    placeholder="City"
+                    fullWidth
+                    {...formik.getFieldProps("city")}
+                />
+                {formik.errors.city && formik.touched.city && (
+                    <div className="text-sm text-red-600 border-2 border-red-600 w-full h-8 bg-red-100 rounded-md flex justify-center items-center">
+                        {formik.errors.city}
+                    </div>
+                )}
+                <TextField
+                    id="gender"
+                    type="text"
+                    label="Gender"
+                    placeholder="Gender"
+                    fullWidth
+                    {...formik.getFieldProps("gender")}
+                />
+                {formik.errors.gender && formik.touched.gender && (
+                    <div>
+                        {formik.errors.gender}
+                    </div>
+                )}
+                <Button variant="contained" type="submit" fullWidth>
+                    Submit
+                </Button>
+            </form>
+        </section>
+    );
+}
